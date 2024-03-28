b7d3d20819da364a9dd667abc34b453309b077da
xachan <121971475+xachan@users.noreply.github.com>
2024-03-28 14:30:29 -0700

    some of animation

--- enemy.gd
+++ enemy.gd
@@ -3,11 +3,18 @@ extends CharacterBody2D
 var speed = 10
 var player_chase = false
 var player = null
+var animatedSprite
 
+func _ready():
+	print("Enemy ready")
+
+	animatedSprite = $AnimatedSprite2D
+	animatedSprite.play("idle")
 
 func _physics_process(delta):
 	if player_chase:
-		position += (player.position -position)/speed
+		position += (player.position - position)/speed
+
 
 
 
@@ -15,8 +22,10 @@ func _on_area_2d_body_entered(body):
 	
 	player = body
 	player_chase = true
+	animatedSprite.play("walk")
 
 
 func _on_area_2d_body_exited(body):
 	player = null
 	player_chase = false
+	animatedSprite.play("idle")
