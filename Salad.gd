49d50cf6fa053020ac27467c1196b26dda42d7b1
titran3 <121971162+titran3@users.noreply.github.com>
2024-04-15 13:43:00 -0700

    added salad animation

--- Salad.gd
+++ Salad.gd
@@ -1,11 +1,22 @@
 extends Area2D
+var state = "idle"
+@export var movement_data: PlayerMovementData
 
 var player_scene = preload("res://player.tscn").instantiate()
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _process(delta):
 	var salads = get_tree().get_nodes_in_group("Salad")
-	animated_sprite_2d.play("idle")
+	if state == "idle":
+		animated_sprite_2d.play("idle")
+	if state == "eaten":
+		player_scene.movement_data.speed = 150
+		await get_tree().create_timer(0.05).timeout
+		animated_sprite_2d.play("eaten")
+		await get_tree().create_timer(1.0).timeout
+		queue_free()
+		await get_tree().create_timer(3.0).timeout
+		player_scene.movement_data.speed = 100
 
 func _on_body_entered(body):
-	queue_free()
+	state = "eaten"
