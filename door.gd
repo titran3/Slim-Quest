243541f6f46cd9cf24f907cf7712b5cbdbc7a942
titran3 <121971162+titran3@users.noreply.github.com>
2024-04-18 13:32:15 -0700

    door texture

--- door.gd
+++ door.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
 @export var next_level = PackedScene
+@onready var sprite_2d = $Sprite2D
+var image = preload("res://cat door open.png")
 
 func _on_body_entered(body):
 	if is_salad_empty():
 		get_tree().change_scene_to_packed(next_level)
+		$Sprite2D.texture = image
 
 func is_salad_empty() -> bool:
 	var salad_nodes = get_tree().get_nodes_in_group("Salad")
