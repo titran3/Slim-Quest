166387a915969d49f68ed43670c5b08861e419b0
Colin <coyee1@s.sfusd.edu>
2024-04-19 14:12:01 -0700

    flipping midair animation finished

--- player.gd
+++ player.gd
@@ -81,13 +81,12 @@ func handle_acceleration(input_axis, delta):
 		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.acceleration * delta)
 		
 func update_animation(input_axis):
-	if is_on_floor():
-		if input_axis != 0:
-			animated_sprite_2d.flip_h = (input_axis < 0)
-			animated_sprite_2d.play("run")
-		else:
-			animated_sprite_2d.play("idle")
+	if input_axis != 0:
+		animated_sprite_2d.flip_h = (input_axis < 0)
+		animated_sprite_2d.play("run")
 	else:
+		animated_sprite_2d.play("idle")
+	if not is_on_floor():
 		animated_sprite_2d.play("jump")
 		
 func apply_air_resistance(input_axis, delta):
